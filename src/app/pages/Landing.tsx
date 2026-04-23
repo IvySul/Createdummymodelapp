a4fb1f830d0261068c5ed235778658e38a5bd773
IvySul <isulliv5@vols.utk.edu>
2026-04-23 14:29:20 -0400

    a

--- src/app/pages/Landing.tsx
+++ src/app/pages/Landing.tsx
@@ -6,10 +6,10 @@ export default function Landing() {
 
   return (
     <div className="bg-white relative size-full min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center gap-8">
+      <div className="flex flex-col items-center gap-2">
         <img src={splashLogo} alt="Occumate" className="w-[440px] max-w-[90vw] h-auto" />
         
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 -mt-10">
           <button
             onClick={() => navigate('/questionnaire/step1')}
             className="bg-[#d9d9d9] h-[38px] rounded-[11px] w-[125px] font-['ABC_Diatype_Rounded:Regular',sans-serif] text-[16px] text-black hover:bg-[#c9c9c9] transition-colors"
